803c0ff4fb0905b681a3b55c72feab5555bd5c13
abhishekbadole8 <abhishekbadole12@gmail.com>
2024-11-04 17:31:38 +0530

    fix: active upskills for index route also, and created a fallback route to redirect to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import UpSkill from './pages/UpSkill';
 import Dashboard from './pages/Dashboard';
 import Learn from './pages/Learn';
@@ -11,11 +11,14 @@ function App() {
       <div className='w-full h-full min-h-screen p-10 flex bg-white-to-light-blue font-sans'>
         <Routes>
           <Route path="/" element={<UpSkill />} />
-          <Route path="/upskill" element={<UpSkill />} />
+          
+          <Route index path="/upskill" element={<UpSkill />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/learn" element={<Learn />} />
           <Route path="/contest" element={<Contest />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
+
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </Router>
